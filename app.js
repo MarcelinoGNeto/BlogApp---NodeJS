3e91739e981ab1b385b1117465b66ac7cc6eed93
MarcelinoGNeto <marcelino.gneto@gmail.com>
2021-09-28 17:52:28 -0300

    Criando projeto, implementando configurações e primeiras rotas

---
+++ app.js
@@ -0,0 +1,26 @@
+//Carregando Módulos
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require("body-parser")
+    const app = express()
+    const admin = require("./routers/admin")
+    //const mongoose = require("mongoose")
+
+//Configuraçãoes
+    //Body Parser(express)
+        app.use(express.urlencoded({extended: true}))
+        app.use(express.json())
+    //Handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars');
+    //Mongoose
+        //
+    //
+//Rotas
+    app.use('/admin', admin)
+
+//Outros
+const PORT = 8081
+app.listen(PORT,() => {
+    console.log("servidor rodando! ")
+})
---
+++ routers/admin.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send("Página principal do painel ADM")
+})
+
+router.get('/posts', (req, res) => {
+    res.send("Página de Posts")
+})
+
+router.get("/categorias", (req, res) => {
+    res.send("Página de Categorias")
+})
+
+
+
+
+
+module.exports = router
