63a85db979f9d56c7598ff76afd946d4d502538e
MarcelinoGNeto <marcelino.gneto@gmail.com>
2021-09-29 18:17:24 -0300

    Adicionando Middleware, configurando sessoes e valindando insercoes de dados

--- app.js
+++ app.js
@@ -6,8 +6,23 @@
     const admin = require("./routers/admin")
     const path = require('path')
     const mongoose = require("mongoose")
+    const session = require("express-session")
+    const flash = require("connect-flash")
 
 //Configuraçãoes
+    //Sessão
+        app.use(session({
+            secret: "cursodenode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    //Middleware
+        app.use((req, res, next) =>{
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next()
+        })
     //Body Parser(express)
         app.use(express.urlencoded({extended: true}))
         app.use(express.json())
