78bd8d25864c7c38718998f7276bc0644ae83eae
MarcelinoGNeto <marcelino.gneto@gmail.com>
2021-10-01 16:33:54 -0300

    Criacao de model Postagem, formulario e salvando no bd

--- routers/admin.js
+++ routers/admin.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const mongoose = require("mongoose")
 require("../models/Categoria")
 const Categoria = mongoose.model("categorias")
-//const bodyParser = require("body-parser") 
+require('../models/Postagem')
+const Postagem = mongoose.model("postagens")
 
 router.get('/', (req, res) => {
     res.render("admin/index")
@@ -96,6 +97,48 @@ router.post("/categorias/deletar", (req, res) => {
         req.flash("error_msg", "Houve um erro ao deletar a categoria")
         res.redirect("admin/categorias")
     })
+})
+
+router.get("/postagens", (req, res) => {
+    res.render("admin/postagens")
+})
+
+router.get("/postagens/add", (req, res) => {
+    Categoria.find().sort({name: 'asc'}).lean(true).then((categorias) => {
+        res.render("admin/addpostagem", {categorias: categorias})
+    }).catch((err) =>{
+        req.flash("error_msg", "Houve um erro ao carregar o formulário")
+        res.redirect("/admin")
+    })
+})
+
+router.post("/postagens/nova", (req, res) => {
+
+    var erros = []
+
+    if (req.body.categoria == "0"){
+        erros.push({texto: "Categoria inválida, registre uma nova categoria"})
+    }
+
+    if (erros.length > 0){
+        res.render("admin/addpostagem", {erros: erros})
+    }else{
+        const novaPostagem = {
+            titulo: req.body.titulo,
+            descricao: req.body.descricao,
+            conteudo: req.body.conteudo,
+            categoria: req.body.categoria,
+            slug: req.body.slug
+        }
+
+        new Postagem(novaPostagem).save().then(() => {
+            req.flash("success_msg", "Postagem criada com sucesso!")
+            res.redirect("/admin/postagens")
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro durante o salvamento da postagem")
+            res.redirect("/admin/postagens")
+        })
+    }
 })
 
 module.exports = router
